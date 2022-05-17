3fd89c9dacf1ec70b5192759c9ab33dd935bd153
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-17 09:23:09 +0900

    05.17

---
+++ src/Array.java
@@ -0,0 +1,22 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i);
+        }
+
+        int arrayInt2[]={1,3,5,7,9};
+        for (int i = 0; i < arrayInt2.length; i++) {
+            System.out.println(i);
+        }
+        int arr3[]={1,3,5,7,9};
+        for(int i=0; i< arr3.length;i++){
+            System.out.println(arr3[i]);
+        }
+        String str[]={"a","b","c","d","f"};
+        for (int i=0; i< str.length;i++){
+            System.out.println(str[i]);
+        }
+    }
+}
+
