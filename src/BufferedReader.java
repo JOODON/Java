d8f9c95041458465a503cdbdcd85805d5f075f2f
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-28 09:48:34 +0900

    제네릭

---
+++ src/BufferedReader.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BufferedReader {
+    public static void main(String[] args) {
+        print(1);
+        print(3);
+        print("안녕");
+        }
+        public static <T> void print(T tyle){
+            System.out.println(tyle);
+        }
+    }
+
+
