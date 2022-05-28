b3a6858ce77e61dfc8775858f6fbe846930ba5d1
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-28 10:16:59 +0900

    어레이 리스트

--- src/ArrayListTest.java
+++ src/ArrayListTest.java
@@ -2,12 +2,29 @@ import java.util.ArrayList;
 
 public class ArrayListTest {
     public static void main(String[] args) {
-    ArrayList<String> names=new ArrayList<String>();
-    names.add("주동호");
-    names.add("이선영");
-    names.add("이건 그냥");
-    for(String e:names){
+    ArrayList<Book> list=new ArrayList<Book>();
+    Book onepice=new Book("원피스",4500);
+    Book naguto=new Book("나루토",2500);
+    list.add(onepice);
+    list.add(naguto);
+    for (Book e:list){
         System.out.println(e);
     }
+}
+}
+class Book{
+    private String name;
+    private int price;
+
+    Book(String name, int price){
+        this.name=name;
+        this.price=price;
+    }
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
