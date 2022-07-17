fe3af935be39bdf4cc000f85bd12136eae53450b
주동호 <100278301+JOODON@users.noreply.github.com>
2022-07-17 11:06:36 +0900

    자바 인터페이스

--- src/Classs.java
+++ src/Classs.java
@@ -1,31 +1,15 @@
 public class Classs{
     public static void main(String[] args) {
-        abc player=new abc("위자드",100);
-        User2 player2=new User2("해리포터",100,30);
-        System.out.println(player.print());
-        System.out.println(player2.print());
+        Test.m1();
+
+
     }
 }
-class abc{
-    protected Object hp;
-    protected Object name;
-
-    public abc(String name,int hp){
-        this.name=name;
-        this.hp=hp;
+class Test{
+    static void m1(){
+        System.out.println("유틸리티 메소드 입니다");
     }
-    public String print(){
-        return String.format("%s (Hp%d),",this.name,this.hp);
-    }
-}
-class User2 extends abc{
-    private int mp;
-
-    public User2(String name, int hp,int mp) {
-        super(name, hp);//기존에 있던걸 가져옴
-        this.mp=mp;
-    }
-    public String print(){
-        return String.format("%s (HP:%d MP:%d)",this.name,this.hp,this.mp);
+    void m2(){
+        System.out.println("일반 메소드 입니다");
     }
 }
---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+//api를 불러줌
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("값을 입력해주세여");
+        int a=s.nextInt();
+        
+        System.out.println(a);
+    }
+}
